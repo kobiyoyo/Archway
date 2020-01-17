b54ac27f8675b899f3e277c80d9fe5f3fe1adb01
kobiyoyo <adamachubi@gmail.com>
2020-01-17 14:21:10 +0100

    added omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -255,7 +255,7 @@ Devise.setup do |config|
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
-
+  config.omniauth :facebook, "App ID", "App Secret", callback_url: "http://localhost:3000/users/auth/facebook/callback"
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
