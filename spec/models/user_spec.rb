f3ac326b8903093642dae3219b00c8fbf5f541e4
danilozag1992 <danilozagarcanin@gmail.com>
2020-02-05 19:26:48 +0100

    fixing lint errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-   describe 'validations' do
+  describe 'validations' do
   	it 'requires email' do 
-      user = User.new(email:' ',password:'chubiyojo',password_confirmation:'chubiyojo') 
+      user = User.new(email:' ',password:'chubiyojo', password_confirmation:'chubiyojo')
       expect(user.valid?).to be_falsy
-  	end
+    end
+
   	it 'requires password' do 
-      user = User.new(email:'yabmas@gmail.com',password:'',password_confirmation:'') 
+      user = User.new(email:'yabmas@gmail.com', password:'', password_confirmation:'')
       expect(user.valid?).to be_falsy
-  	end
-  	it 'requires password_confirmation' do 
-      user = User.new(email:'yabmas@gmail.com',password:'chubiyojo',password_confirmation:'') 
+    end
+
+  	it 'requires password_confirmation' do
+      user = User.new(email:'yabmas@gmail.com', password:'chubiyojo', password_confirmation:'')
       expect(user.valid?).to be_falsy
-  	end
-  	it { should have_many(:posts) }
+    end
+
+    it { should have_many(:posts) }
   end
 end
+
+
+
