50b093a178658bc4cd12dede674c068498f439c1
danilozag1992 <danilozagarcanin@gmail.com>
2020-02-16 15:23:36 +0100

    Added adding friends, even possible to see on other side that they become friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,8 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def show
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
             resources :comments
             resources :likes
         end
-        
+        resources :friendships
 
 end
