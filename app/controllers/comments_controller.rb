812cea104418d7b30e4149cd7f8ca8d424fb9aab
kobiyoyo <adamachubi@gmail.com>
2020-02-24 00:55:52 +0100

    refactored comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,33 @@
 class CommentsController < ApplicationController
   before_action :find_post, only: [:create]
-
+  before_action :set_comment, only: %i[destroy]
+  def new 
+    @comment = @post.comments.build
+  end
   def create
-    @comment = @post.comments.new(comment_params)
+    @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
-    @comment.save
-    redirect_to root_path
+    if @comment.save
+      redirect_to root_path,success: 'Comment created'
+    else
+      redirect_to @post ,alert: 'Comment not created'
+    end
   end
 
+  def destroy
+    @comment.destroy
+    flash[:danger] = 'Comment was successfully deleted'
+    redirect_to post_path
+  end
+  
   private
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id]) 
   end
 
   def find_post
