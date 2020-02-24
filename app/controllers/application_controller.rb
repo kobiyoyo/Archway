72553d3bcf2050472044070d8443e8bbf19c7db6
kobiyoyo <adamachubi@gmail.com>
2020-02-24 02:09:00 +0100

    moved logic from post views to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :instantiate_post
+  before_action :instantiate_comment
 
+  def instantiate_post
+    @post = Post.new
+  end
+
+  def instantiate_comment
+    @comment = Comment.new
+  end
   protected
 
   def configure_permitted_parameters
