9c0c2f0d8c80a483dada3dab3bd89886f202aa1c
danilozag1992 <danilozagarcanin@gmail.com>
2020-02-25 11:52:54 +0100

    fixed lint errors

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,19 +1,19 @@
 class FriendshipsController < ApplicationController
+
   def create
     @friendship = current_user.friendships.build(friend_id: params[:friend_id])
     if @friendship.save
-      flash[:notice] = "Added friend."
-      redirect_to root_url
+      flash[:notice] = 'Added friend.'
     else
-      flash[:notice] = "Unable to add friend"
-      redirect_to root_url
+      flash[:notice] = 'Unable to add friend'
     end
+    redirect_to root_url
   end
 
   def destroy
     @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
-    flash[:notice] = "Removed friendship."
+    flash[:notice] = 'Removed friendship.'
     redirect_to current_user
   end
 end
