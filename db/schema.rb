5c8356629ea999c5df5a0a2f9d3f33155b882747
kobiyoyo <adamachubi@gmail.com>
2020-02-25 08:33:17 +0100

    added friendship to route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_115244) do
+ActiveRecord::Schema.define(version: 2020_02_24_113011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2020_02_10_115244) do
     t.bigint "post_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "likes", force: :cascade do |t|
@@ -53,7 +60,6 @@ ActiveRecord::Schema.define(version: 2020_02_10_115244) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
-    t.string "string"
     t.string "gender"
     t.string "first_name"
     t.string "last_name"
